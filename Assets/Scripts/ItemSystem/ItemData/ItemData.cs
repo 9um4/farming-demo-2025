78dd7fca0f9a1e43ed193ae855f068d2799753be
9um4 <51898564+9um4@users.noreply.github.com>
2025-04-27 14:47:36 +0900

    Add: create abstract ItemData class for item systems

---
+++ Assets/Scripts/ItemSystem/ItemData/ItemData.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace ItemSystem.ItemData
+{
+    public abstract class ItemData: MonoBehaviour
+    {
+        #region Common Data
+        [Header("Common Data")]
+        [SerializeField] private Material material;
+        [SerializeField] private string itemName;
+        [SerializeField] private string itemDescription;
+        #endregion
+    }
+
+}
+
