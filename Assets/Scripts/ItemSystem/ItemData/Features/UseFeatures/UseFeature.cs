a2b18e06d575482df308160d42d19022e9f22450
9um4 <51898564+9um4@users.noreply.github.com>
2025-04-27 14:53:03 +0900

    Add: create UseFeature for item usage functionality

---
+++ Assets/Scripts/ItemSystem/ItemData/Features/UseFeatures/UseFeature.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace ItemSystem.ItemData.Features.UseFeatures
+{
+    /// <summary>
+    /// 아이템 사용 기능을 나타내는 인터페이스
+    /// </summary>
+    public interface IUseFeature
+    {
+
+    }
+}
