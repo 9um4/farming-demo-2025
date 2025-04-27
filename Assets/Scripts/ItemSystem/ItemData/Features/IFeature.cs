20af289081420cadd3005e628c4b41df417b4647
9um4 <51898564+9um4@users.noreply.github.com>
2025-04-27 14:52:26 +0900

    Add: create IFeature interface to define item functionalities

---
+++ Assets/Scripts/ItemSystem/ItemData/Features/IFeature.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace ItemSystem.ItemData.Features
+{
+    /// <summary>
+    /// Item의 기능을 정의하는 인터페이스
+    /// </summary>
+    public interface IFeature
+    {
+        
+    }
+}
